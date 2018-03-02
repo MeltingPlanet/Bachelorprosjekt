e13c2484edbf2a1fabff263b8ac0e2105c4e6b6f
MeltingPlanet <36302354+MeltingPlanet@users.noreply.github.com>
2018-03-02 17:39:00 +0100

    update

--- Script/nettside.js
+++ Script/nettside.js
@@ -1,76 +1,61 @@
 console.log(ambisonics);
 
-//Setter opp audio context og variabler
-//webkit for safari og eldre versjoner av chrome
-
+// AUDIO CONTEXT & WEBKIT ##############################################################################
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var context = new AudioContext; //Initialiserer audio context
 
-var soundUrl = "Lyder/wxyz4.wav";
+// SPECIFY FILTER URL ###################################################################################
 var FilterUrl = "Filter/aalto2016_N1.wav";
 
-var soundBuffer, sound;
 
-// ambisonics rotasjon
+// AMBISONICS ROTATION #################################################################################
 var rotator = new ambisonics.sceneRotator(context, 1); // 1. orden (FOA)
 console.log(rotator);
 
-//ambisonic decoder
+
+//AMBISONIC DECODER ####################################################################################
 var binDecoder = new ambisonics.binDecoder(context, 1);
 console.log(binDecoder);
 
-//FuMa (b-format rekkefølge til ACN <--> WXYZ til WYZX)
+
+//FuMa (b-format rekkefølge til ACN <--> WXYZ til WYZX) ################################################
 var converterF2A = new ambisonics.converters.wxyz2acn(context);
 console.log(converterF2A);
 
-// output gain
+
+// OUTPUT GAIN #########################################################################################
 var gainOut = context.createGain();
 
-//Signalgang-kobling (FOA --> Rotering --> Bineaural --> Gain --> Destinasjon/ut)
+
+//SIGNAL FLOW (FOA --> Rotering --> Bineaural --> Gain --> Destinasjon/ut) #############################
 converterF2A.out.connect(rotator.in);
 rotator.out.connect(binDecoder.in);
-binDecoder.out.connect(gainOut);
-gainOut.connect(context.destination);
+binDecoder.out.connect(context.destination);
 
-// function to load samples
+
+// LOAD SAMPLE #########################################################################################
 function loadSample(url, doAfterLoading) {
-    var fetchSound = new XMLHttpRequest(); // Load the Sound with XMLHttpRequest, objekt som henter data fra server uten å refreshe siden (som messenger på facebook)
-    fetchSound.open("GET", url, true); // Path to Audio File, initialiserer request
+    var fetchSound = new XMLHttpRequest(); // Load the Sound with XMLHttpRequest
+    fetchSound.open("GET", url, true); // Path to Audio File
     fetchSound.responseType = "arraybuffer"; // Read as Binary Data
-    fetchSound.onload = function() { // onload <- event handler
+    fetchSound.onload = function() {
         context.decodeAudioData(fetchSound.response, doAfterLoading, onDecodeAudioDataError);
     }
-    fetchSound.send(); //asynkron request sendes
+    fetchSound.send(); //
 }
 
-// function to assign sample to the sound buffer for playback (and enable playbutton)
-var assignSample2SoundBuffer = function(decodedBuffer) {
-        soundBuffer = decodedBuffer;
-        document.getElementById('play').disabled = false;
-    }
-    // function to assign sample to the filter buffers for convolution
+// SAMPLE TO CONVOLUTION FILTER #################################################################
 var assignSample2Filters = function(decodedBuffer) {
     binDecoder.updateFilters(decodedBuffer);
 }
-// function to change sample from select box
-function changeSample() {
-    document.getElementById('play').disabled = true;
-    document.getElementById('stop').disabled = true;
-    soundUrl = document.getElementById("sample_no").value;
-    if (typeof sound != 'undefined' && sound.isPlaying) {
-        sound.stop(0);
-        sound.isPlaying = false;
-    }
-    loadSample(soundUrl, assignSample2SoundBuffer);
-}
 
-// load and assign samples
-loadSample(soundUrl, assignSample2SoundBuffer);
+// LOAD SAMPLES #############################################################################
+
 loadSample(FilterUrl, assignSample2Filters);
 
 
+// MOBIL SENSOR ROTERING #############################################################################
 var rotasjonSlider = document.getElementById("grader");
-
 var initialOffset = null;
 
 window.addEventListener("deviceorientation", function(event) {
@@ -95,37 +80,50 @@ window.addEventListener("deviceorientation", function(event) {
     rotator.updateRotMtx();
 }, false);
 
-
-var volumeSlider = document.getElementById('volume-slider');
-volumeSlider.addEventListener('input', function(input) {
-    gainOut.gain.value = volumeSlider.value;
-});
-
-
+// READY FUNCTION - PLAY STOP ############################################################################
 $(document).ready(function() {
 
-    // Init event listeners
-    document.getElementById('play').addEventListener('click', function() {
-        sound = context.createBufferSource();
-        sound.buffer = soundBuffer;
-        sound.loop = true;
-        sound.connect(converterF2A.in);
-        sound.start(0);
-        sound.isPlaying = true;
-        document.getElementById('play').disabled = true;
-        document.getElementById('stop').disabled = false;
-    });
-    document.getElementById('stop').addEventListener('click', function() {
-        sound.stop(0);
-        sound.isPlaying = false;
-        document.getElementById('play').disabled = false;
-        document.getElementById('stop').disabled = true;
-    });
-
+    for(var i = 1; i <=2; i ++){
+        var audioElement = document.getElementById("audioElement" + i);
+        console.log("audioElement" + i)
+        audioElement.loop = true;
+     
+        var mediaElementSource = context.createMediaElementSource(audioElement);
+        console.log(mediaElementSource);
+     
+        mediaElementSource.connect(converterF2A.in);
+    };
 });
 
-// function called when audiocontext.decodeAudioData fails to decode a given audio file, e.g. in Safari with .ogg vorbis format
+// EXCEPTION ALERT ###########################################################################
 function onDecodeAudioDataError(error) {
     var url = 'hjre';
   alert("Browser cannot decode audio data..." + "\n\nError: " + error + "\n\n(If you re using Safari and get a null error, this is most likely due to Apple's shady plan going on to stop the .ogg format from easing web developer's life :)");
 }
+
+
+/*#####################################################################
+  _.,-----/=\-----,._ ##
+ (__ ~~~"""""""~~~ __)##    '########:'########:::::'###:::::'######::'##::::'##:
+  | ~~~"""""""""~~~ | ##    ... ##..:: ##.... ##:::'## ##:::'##... ##: ##:::: ##:
+  | |  ; ,   , ;  | | ##    ::: ##:::: ##:::: ##::'##:. ##:: ##:::..:: ##:::: ##:
+  | |  | |   | |  | | ##    ::: ##:::: ########::'##:::. ##:. ######:: #########:
+  | |  | |   | |  | | ##    ::: ##:::: ##.. ##::: #########::..... ##: ##.... ##:
+  | |  | |   | |  | | ##    ::: ##:::: ##::. ##:: ##.... ##:'##::: ##: ##:::: ##:
+  | |  | |   | |  | | ##    ::: ##:::: ##:::. ##: ##:::: ##:. ######:: ##:::: ##:
+  | |  | |   | |  | | ##    :::..:::::..:::::..::..:::::..:::......:::..:::::..::
+  | |  | |   | |  | | ##
+  |. \_| |   | |_/ .| ##
+   `-,.__ ~~~ __.,-'  ##
+         ~~~~~        ##
+#######################################################################
+
+
+
+
+
+
+
+#######################################################################
+ */
+
