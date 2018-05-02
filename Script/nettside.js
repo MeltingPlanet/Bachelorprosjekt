06521759677c34efea344e79ab34c2ae5e2fe740
MeltingPlanet <36302354+MeltingPlanet@users.noreply.github.com>
2018-05-02 14:26:30 +0200

    visual update

--- Script/nettside.js
+++ Script/nettside.js
@@ -1,15 +1,15 @@
 console.log(ambisonics);
 
 // AUDIO CONTEXT & WEBKIT ##############################################################################
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var context = new AudioContext; //Initialiserer audio context
+var context = new (window.AudioContext || window.webkitAudioContext)();
+//var context = new AudioContext; //Initialiserer audio context
+
 
 // SPECIFY FILTER URL ###################################################################################
 var FilterUrl = "Filter/aalto2016_N1.wav";
 
 
 // AMBISONICS ROTATION #################################################################################
-
 var rotator = new ambisonics.sceneRotator(context, 1); // 1. orden (FOA)
 
 //AMBISONIC DECODER ####################################################################################
@@ -24,9 +24,7 @@ var converterF2A = new ambisonics.converters.wxyz2acn(context);
 console.log(converterF2A);
 
 // MONO ENCODER #######################################################################################
-var MonoAzim = 0;
-var MonoElev = 0;
-var monoEncoder = new ambisonics.monoEncoder(context, 1, MonoAzim, MonoElev);
+var monoEncoder = new ambisonics.monoEncoder(context, 1);
 console.log(monoEncoder);
 
 // OUTPUT GAIN #########################################################################################
@@ -62,6 +60,9 @@ var assignSample2Filters = function(decodedBuffer) {
 loadSample(FilterUrl, assignSample2Filters);
 
 //########################### AJAX med JSON #############################
+
+//
+//
 var select = document.getElementById("audioFile");
 var lydvalg = document.getElementById("lydvalg");
 
@@ -78,6 +79,7 @@ if(select.value != "notselect"){
   ourRequest.send(); 
 } 
 });
+
 
 function changeAudio(lydfil){
   var divElement = document.createElement("div");
@@ -120,15 +122,67 @@ function changeAudio(lydfil){
     mediaElementSource.connect(converterF2A.in);
     }else{
     mediaElementSource.connect(monoEncoder.in);
-    }
+  }
   }
 }
 
 var checkbox = document.getElementById("checkBox");
 
 // MOBIL SENSOR ROTERING #############################################################################
+function lydRotasjon(pitch, yaw) {
 
-lydvalg.addEventListener("click", function(){
+      // Oppdatere Ambisonics rotasjon
+      // 
+      rotator.yaw = yaw;
+      rotator.pitch = pitch;
+      rotator.updateRotMtx();
+
+};
+
+// ############## MonoEncoder plassering ##############################################################
+
+monoEncoder.azim = 0;
+monoEncoder.elev = 0;
+monoEncoder.updateGains();
+
+/*
+var gn = new GyroNorm();
+
+checkbox.addEventListener("click", function(){
+  gn.init().then(function(){
+    gn.start(function(data){
+      var yaw = data.do.alpha; // Rotasjon rundt Z-aksen (0 - 360 grader) 
+      var pitch = data.do.beta + 180; // Rotasjon rundt x-aksen (-180 til 180 grader) 
+      var roll = data.do.gamma; // Rotasjon rundt y-aksen (-90 til 90 grader)
+
+      //console.log(yaw, pitch, roll);
+
+      // Oppdatere Ambisonics rotasjon
+      // 
+      rotator.yaw = yaw;
+      rotator.pitch = pitch;
+      rotator.roll = roll;
+      rotator.updateRotMtx();
+      
+      if(checkbox.checked == true){
+
+        var rotasjonSlider = document.getElementById("Slider");
+        rotasjonSlider.value = yaw;
+        rotator.yaw = yaw;
+        rotator.pitch = pitch;
+        rotator.roll = roll;
+        rotator.updateRotMtx();
+      };
+
+    });
+  }).catch(function(e){
+    console.log("not supported");
+  })
+});
+
+*/
+/*
+checkbox.addEventListener("click", function(){
   window.addEventListener("deviceorientation", function(){
       var alpha = event.alpha;
       if(alpha < 0) {
@@ -144,16 +198,20 @@ lydvalg.addEventListener("click", function(){
       };
   });
 });
+*/
 // READY FUNCTION - PLAY STOP ############################################################################
 
 // AUDIO VISUALIZATION #######################################################################
 // 
+//
+
+
 function drawLocal() {
     // Update audio analyser buffers
     analyser.updateBuffers();
     var params = analyser.computeIntensity();
     updateCircles(params, canvas2);
-    console.log(params);
+    update3d(params);
 }
 
 
@@ -164,7 +222,7 @@ mapSprite.src = "Bilder/map.png";
 var canvas2 = document.getElementById('Canvas2');
 var canvas2_context = canvas2.getContext("2d");
 var circles = [];
-var numCircleLim = 100;
+var numCircleLim = 10;
 var opacityLim = 0.2;
 
 function Circle(xPos, yPos, radius, opacity) {
@@ -209,6 +267,11 @@ function updateCircles(params, cnv) {
     }
 }
 
+function del3d() {
+
+};
+
+
 function draw() {
     requestAnimationFrame(draw);
 
@@ -220,20 +283,94 @@ function draw() {
     canvas2_context.drawImage(mapSprite, 0, 0);
 
         // run example specific draw callback
-    if (!(typeof(drawLocal)=='undefined')) { drawLocal(); }
+    //if (!(typeof(drawLocal)=='undefined')) { drawLocal(); }
     // Draw Circles
+    drawLocal();
 
     for (var i=0; i<circles.length; i++) circles[i].draw(canvas2_context);
 
 };
 draw();
 
+// A-FRAME ###################################################################################
+
+function update3d(params) {
+    var sceneEl = document.querySelector('a-scene');
+    var sphereEl = document.createElement('a-sphere');
+
+    var sphereX = params[0]; // azim
+    var sphereY = params[1]; // elev
+
+    var sphereY = sphereY - 90;
+    //console.log(sphereX, sphereY);
+
+    // ########### konvertering #####################
+
+    radius = 60
+    radSphereX = sphereX * Math.PI/180; // Radianer
+    radSphereY = sphereY * Math.PI/180;
+
+    cartX = radius * Math.cos(radSphereY) * Math.cos(radSphereX); // Kartesiske koordinatsystem
+    cartY = radius * Math.cos(radSphereY) * Math.sin(radSphereX);
+    cartZ = radius * Math.sin(radSphereY);
+
+    //###############################################
+    var cartX = cartX.toString();
+    var cartY = cartY.toString();
+    var cartZ = cartZ.toString();
+
+    var sphereHolder = cartX + " " + cartY + " " + cartZ;
+    //console.log(sphereHolder);
+
+    //var radi = 30*(1-params[2]);
+
+        var circle = document.createElement('a-sphere');
+        sphereEl.setAttribute("position", sphereHolder);
+        sphereEl.setAttribute("color", "#00FFFF");
+        sceneEl.appendChild(sphereEl);
+        //sphereEl.setAttribute("radius", radi);
+        var animator = document.createElement("a-animation");
+        animator.setAttribute("attribute", "radius");
+        animator.setAttribute("dur", "200");
+        var intensity = 15*(1-params[2]);
+        var intensity = String(intensity);
+        //console.log(intensity);
+        animator.setAttribute("from", intensity);
+        animator.setAttribute("to", "1");
+        animator.setAttribute("direction", "normal");
+        sphereEl.appendChild(animator);
+        animator.setAttribute("begin", "fade")
+        sphereEl.emit("fade")
+
+        var counter = sceneEl.childElementCount;
+        if (counter > 100) {
+          //sceneEl.removeChild(sceneEl.firstChild);
+          var sphere = document.querySelector('a-sphere');
+          sphere.parentNode.removeChild(sphere);
+
+        var camera = document.querySelector('[camera]').object3D.rotation
+
+        //skjekk riktig xyz
+        console.log(document.querySelector('[camera]').object3D);
+        var camX = document.querySelector('[camera]').object3D.rotation._x;
+        var camY = document.querySelector('[camera]').object3D.rotation._y;
+        var camZ = document.querySelector('[camera]').object3D.rotation._z;
+        //console.log(camY);
+        var camPitch = camX*180/Math.PI;
+        var camYaw = camY*180/Math.PI;
+        var camRoll = camZ*180/Math.PI;
+        console.log(camPitch);
+        //console.log(camPitch, camYaw, camZ);
+        lydRotasjon(camPitch, camYaw);
+        };
+};
+
+
 // EXCEPTION ALERT ###########################################################################
 function onDecodeAudioDataError(error) {
     var url = 'hjre';
   alert("Browser cannot decode audio data..." + "\n\nError: " + error + "\n\n(If you re using Safari and get a null error, this is most likely due to Apple's shady plan going on to stop the .ogg format from easing web developer's life :)");
-}
-
+};
 
 /*#####################################################################
   _.,-----/=\-----,._ ##
@@ -277,5 +414,23 @@ function onDecodeAudioDataError(error) {
 
 
 //#######################################################################
- 
+ /*
 
+AFRAME.registerComponent('listener', {
+  tick: function () {
+    console.log(this.el.getAttribute('position'));
+  }
+});
+
+AFRAME.registerComponent('rotation-reader', {
+  tick: function () {
+    console.log(this.el.getAttribute('rotation'));
+  }
+});
+
+tick: function () {
+var camera = document.querySelector('[camera]').object3D
+console.log(camera)  
+}
+
+*/
